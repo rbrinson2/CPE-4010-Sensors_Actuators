db9a78d900ac0b36c3b248c1e8722f9456d2aeb7
rbrin <rbrinson2@gmail.com>
2024-10-09 15:47:29 -0400

    Found the tone() function

--- Lab5/Lab5.ino
+++ Lab5/Lab5.ino
@@ -6,7 +6,7 @@ const int FREQ_MIN = 60;
 const int PERIOD_60HZ = 16667; // microseconds
 const int PERIOD_10KHZ = 100; // microseconds
 
-const float SLOPE = float(PERIOD_10KHZ - PERIOD_60HZ) / float(POT_MAX);
+const float SLOPE = float(FREQ_MAX - FREQ_MIN) / float(POT_MAX);
 
 
 int buzzer=8;// select digital IO pin for the buzzer
@@ -22,24 +22,15 @@ void setup() {
 void loop() { 
   unsigned char i,j;//define variable
   float val = 0;
-  int period = 0;
+  int freq = 0;
   while(1) 
   { 
     val = analogRead(pot);
-    period = int(SLOPE * val + PERIOD_60HZ);
+    freq = int(SLOPE * val + FREQ_MIN);
+    Serial.print("Frequency = ");
+    Serial.print(freq);
+    Serial.println("Hz");
 
-    Serial.print("Period: ");
-    Serial.print(period);
-    Serial.print("us\tAnalogue Value: ");
-    Serial.println(val);
-
-    for (int i = 0; i < period; i++) {
-      digitalWrite(buzzer,HIGH);// sound
-      delayMicroseconds(1);
-      digitalWrite(buzzer,LOW);//not sound
-      delayMicroseconds(1);
-    }
-    
-    
+    tone(buzzer, freq, 250);
   } 
 } 
