3631f8a5f317c35c7be95679a98b404738b98017
rbrin <rbrinson2@gmail.com>
2024-10-09 15:51:15 -0400

    Cleaned up code

--- Lab5/Lab5.ino
+++ Lab5/Lab5.ino
@@ -3,10 +3,8 @@ const int POT_MAX = 1023;
 const int POT_MIN = 0;
 const int FREQ_MAX = 10000;
 const int FREQ_MIN = 60;
-const int PERIOD_60HZ = 16667; // microseconds
-const int PERIOD_10KHZ = 100; // microseconds
 
-const float SLOPE = float(FREQ_MAX - FREQ_MIN) / float(POT_MAX);
+const float SLOPE = float(FREQ_MAX - FREQ_MIN) / float(POT_MAX - POT_MIN);
 
 
 int buzzer=8;// select digital IO pin for the buzzer
@@ -20,17 +18,23 @@ void setup() {
 
 
 void loop() { 
-  unsigned char i,j;//define variable
+  
   float val = 0;
   int freq = 0;
   while(1) 
   { 
     val = analogRead(pot);
+
+    // Quick equation to find the frequency given the 
+    // setting of the potentieometer
     freq = int(SLOPE * val + FREQ_MIN);
+
+    // Print the resulting frequency
     Serial.print("Frequency = ");
     Serial.print(freq);
     Serial.println("Hz");
 
+    // Function to output the desired frequency
     tone(buzzer, freq, 250);
   } 
 } 
