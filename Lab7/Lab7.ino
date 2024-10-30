c3fe2e048e5d122e5512c03340f27f4986f9da9a
rbrin <rbrinson2@gmail.com>
2024-10-30 13:21:18 -0400

    Finished Lab7

--- Lab7/Lab7.ino
+++ Lab7/Lab7.ino
@@ -5,8 +5,13 @@
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-uint8_t count = 1;
+int waterSensorPin = 7;
 
+void firstLine(uint8_t offset){
+  uint8_t colOffset = 0;
+  uint8_t rowOffset = 0;
+  lcd.setCursor(colOffset, rowOffset + offset);
+}
 void secondLine(uint8_t offset){
   uint8_t colOffset = 0;
   uint8_t rowOffset = 1;
@@ -16,6 +21,7 @@ void secondLine(uint8_t offset){
 
 void setup()
 {
+  Serial.begin(9600);
 	// initialize the LCD
 	lcd.begin();
 
@@ -26,7 +32,39 @@ void setup()
 }
 
 void loop()
-{
+{ 
+  int waterLevel = analogRead(waterSensorPin);
+  Serial.println(waterLevel);
+
+  
+  if (waterLevel <= 1){
+    firstLine(0);
+    lcd.clear();
+    lcd.print("Dry");
+  }
+  else if (waterLevel > 0 && waterLevel < 600) {
+    firstLine(0);
+    lcd.clear();
+    lcd.print("Low");
+  }
+  else if (waterLevel >= 600 && waterLevel < 700) {
+    firstLine(0);
+    lcd.clear();
+    lcd.print("Medium");
+  }
+  else if (waterLevel >= 700) {
+    firstLine(0);
+    lcd.clear();
+    lcd.print("Full");
+  }
+  else {
+    firstLine(0);
+    lcd.clear();
+    lcd.print("Error");
+  }
+
   secondLine(0);
-  lcd.print("%c", count);
+  lcd.print(waterLevel);
+
+  delay(1000);
 }
