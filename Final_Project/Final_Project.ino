267aca7f9e8fd07f002fd972154e8920fdfc2c9f
rbrin <rbrinson2@gmail.com>
2024-11-12 10:30:15 -0500

    Added servo support

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Servo.h"
 
+#define SERVO_PIN 2
+
+Servo head_servo;
+
+void setup() {
+  Serial.begin(9600);
+  head_servo.attach(SERVO_PIN);
 }
 
+// --------------- Functions -------------------- //
+
+// --------------------------------- Head Turn -- //
+void turn_head(int angle){
+  head_servo.write(angle);
+}
+
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+
 
 }
