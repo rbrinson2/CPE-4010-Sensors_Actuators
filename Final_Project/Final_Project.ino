1786902f58e2f57f78b9d0cc4dfcd3bc03c7b4ee
rbrin <rbrinson2@gmail.com>
2024-11-12 11:58:32 -0500

    Added dht11 support

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -1,24 +1,41 @@
 #include "Servo.h"
+#include "dht11.h"
 
 #define SERVO_PIN 2
 
+
+// --------------- Global Variables ------------- //
 Servo head_servo;
 
+
+
+
+
+// --------------- Functions -------------------- //
+
+// ---------------------------------------------- Setup 
 void setup() {
   Serial.begin(9600);
   head_servo.attach(SERVO_PIN);
 }
 
-// --------------- Functions -------------------- //
-
-// --------------------------------- Head Turn -- //
-void turn_head(int angle){
+// ---------------------------------------------- Head Turn
+void Turn_Head(int angle){
   head_servo.write(angle);
+}
+
+// ---------------------------------------------- Detect Distance
+int Detect_Distance(){
+  int d = 0;
+  return d;
 }
 
 
 
 void loop() {
 
+  
+
+  delay(50);
 
 }
